18a1184fff4541e96d8c6e6aef41684cf1adfa0e
npthao110 <n.p.thao.110@gmail.com>
2025-05-02 15:00:16 -0500

    Challenge 4

--- index.js
+++ index.js
@@ -64,20 +64,24 @@ addGamesToPage(GAMES_JSON);
 // grab the contributions card element
 const contributionsCard = document.getElementById("num-contributions");
 
-// use reduce() to count the number of total contributions by summing the backers
+// use reduce() to count the number of total contributions
+const totalContributions = GAMES_JSON.reduce((acc, game) => acc + game.backers, 0);
 
+// set the inner HTML
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
-// set the inner HTML using a template literal and toLocaleString to get a number with commas
-
-
-// grab the amount raised card, then use reduce() to find the total amount raised
+// grab the amount raised card
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+// use reduce() to calculate total pledged
+const totalRaised = GAMES_JSON.reduce((acc, game) => acc + game.pledged, 0);
 
+// set the inner HTML
+raisedCard.innerHTML = `$${totalRaised.toLocaleString()}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+gamesCard.innerHTML = `${GAMES_JSON.length}`;
 
 
 /*************************************************************************************
